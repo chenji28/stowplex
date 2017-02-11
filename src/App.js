b62673a78951557c6b3872dcd44e38eab8addab9
Nick Petrus <nick@atlas.co>
2017-02-11 14:25:56 -0800

    Fixes bootstrap nav

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import Navbar from './components/Navbar.js'
+import Header from './components/Header.js'
 import Footer from './components/Footer.js'
 
-import logo from './logo.svg';
 import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <section className="container">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <br/>
-          <br/>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <br/>
-          <br/>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <br/>
-          <br/>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <br/>
-          <br/>
-        </section>
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <Header />
+                <section className="container">
+
+                </section>
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import logo from '../logo.svg';
+import {Navbar, MenuItem, NavDropdown, Nav, NavItem} from 'react-bootstrap';
+
+class Header extends Component {
+  render() {
+    return (
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <img lat="Stowaway" src="{logo}" />
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavItem eventKey={2} href="#">Link</NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
+    );
+  }
+}
+
+export default Header;
